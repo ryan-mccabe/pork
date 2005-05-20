2fa9e72a124b01a486473763f802d04c6813b752
odin <odin>
2005-05-20 15:30:37 +0000

    *** empty log message ***

---
+++ src/perl/pork_perl_transfer.c
@@ -0,0 +1,41 @@
+/*
+** pork_perl_transfer.c - Perl scripting support
+** Copyright (C) 2005 Ryan McCabe <ryan@numb.org>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License, version 2,
+** as published by the Free Software Foundation.
+*/
+
+#include <config.h>
+
+#include <unistd.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#undef PACKAGE
+#undef instr
+
+#ifndef HAS_BOOL
+#	define HAS_BOOL
+#endif
+
+#include <EXTERN.h>
+#include <perl.h>
+#include <XSUB.h>
+
+#include <pork.h>
+#include <pork_missing.h>
+#include <pork_util.h>
+#include <pork_list.h>
+#include <pork_screen.h>
+#include <pork_transfer.h>
+#include <pork_perl_xs.h>
+
+/* Soon. */
